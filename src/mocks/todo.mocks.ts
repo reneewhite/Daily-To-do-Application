58b5bd643059701b136cbf1f22db4a1eb6e529e7
reneewhite <reneilwesefora@gmail.com>
2018-07-12 13:06:29 +0200

    first commit

--- src/mocks/todo.mocks.ts
+++ src/mocks/todo.mocks.ts
@@ -1,8 +1,8 @@
-const Todowork: Todo[] = [({description: "Wash the dishes",priority: "Low ", status:"Not Done "}),
-                         ({description: "Clean the house",priority: "Medium", status:"almost Done "}),
+const Todowork: Todo[] = [({description: "Wash the dishes",priority: "Low ", status:"Done "}),
+                         ({description: "Clean the house",priority: "Medium", status:"Done "}),
                          ({description: "Study for Exam",priority: "High ", status:"Done "}),
-                         ({description: "Go to church",priority: "Medium ", status:"Done "}),
-                         ({description: "Prepare super",priority: "High ", status:"Almost Done "})];
+                         ({description: "Go to church",priority: "Medium ", status:"Not Done "}),
+                         ({description: "Prepare super",priority: "High ", status:"Done "})];
 
 
 export const TODOWORK = Todowork;
